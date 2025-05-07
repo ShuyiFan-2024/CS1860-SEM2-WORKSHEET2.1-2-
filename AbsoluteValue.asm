8a3228596fc29e38fddda4b4a59775b964397c9b
ShuyiFan-2024 <qnwc5470@leeds.ac.uk>
2025-05-07 10:22:12 +0000

    111

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -1,48 +1,48 @@
 //Initialize R2 and R3
-@R2       
+@R2       //Tell the CPU that it will access RAM[2]
 M=0      //R2=0, suppose x is negative
-@R3      
+@R3      //Access RAM[3]
 M=0      //R3=0, suppose that the absolute value of x can be calculated
 
 //Read X and check if it is negative
-@R0          
+@R0            //Access RAM[0]
 D=M             //D=R0(x)
-@NEGATIVE       
-D;JLT           //if x<0, jump to NEGATIVE
+@NEGATIVE       //Jump to the destination
+D;JLT           //Jump condition, if x<0, jump to NEGATIVE
 
 //Handle the case where X is a positive number
-(POSITIVE)
-@R0
+(POSITIVE)     //Label
+@R0           //Access RAM[0]
 D=M           //D=R0=x
-@R1
+@R1           //Access RAM[1]
 M=D           //R1=x
-@END
+@END          //Jump to the destination
 0;JMP         //unconditional jump         
 
 //Handle the case where X is a negative number
-(NEGATIVE)
-@R2
+(NEGATIVE)    //Label
+@R2         //Access RAM[2]
 M=1         //R2=1, it indicates that x is negative
-@R0
+@R0         //access RAM[0]
 D=M         //D=R0=x, read x
-@MIN_VALUE
-D;JEQ        //if x==-32768, jump to MIN_VALUE
+@MIN_VALUE   //Jump to the destination
+D;JEQ        //Jump condition, if x==-32768, jump to MIN_VALUE
 
 //Calculate the absolute value x (when x is negative)
-D=!D
-D=D+1
-@R1
+D=!D        //Take the opposite
+D=D+1       //and then add one
+@R1         //Access RAM[1]
 M=D       //R1=-x
-@END
-0;JMP
+@END        //Jump to the destination
+0;JMP       //Unconditional jump
 
 //Handling special cases: x==-32768
-(MIN_VALUE)
-@R3
+(MIN_VALUE)     //Label
+@R3         //Access RAM[3]
 M=1        //R3=1, it indicates that the absolute value x is out of range
-@R1
+@R1         //Access RAM[1]
 M=0        //R1=0, since the absolute value x cannot be expressed
 
-(END)
-@END
-0;JMP
+(END)      //Label
+@END       //Jump to the destination
+0;JMP      //Unconditional jump
--- Xor.asm
+++ Xor.asm
@@ -1,28 +1,28 @@
 // let R0=x, R1=y, the result is stored in R2
 // step1: compute x or y
-@R0         
+@R0            //Tell the CPU that it will access RAM[0]
 D=M            //D=R0(x)    
-@R1     
+@R1            //Access RAM[1]
 D=D|M         //D=R0 or R1 (x or y)  
 @R3           //the address of the R3 temporary variable,R3=x or y
-M=D  
+M=D           //R3=x or y
 //step2: compute x and y
-@R0      
+@R0           //Access RAM[0]
 D=M          //D=R0
-@R1     
+@R1           //Access RAM[1]
 D=D&M         //R0 and R1(x and y)
-@R4     
+@R4           //Access RAM[4]
 M=D           //R4=x and y
 //step3: compute not(x and y)
-@R4          //D=R4
-D=M 
+@R4          //Access RAM[4]
+D=M          //D=R4
 D=!D        //D= not(x and y)
-@R4   
+@R4          //Access RAM[4]
 M=D          //R4= not(x and y)
 //step4: compute (x or y) and not(x and y)
-@R3   
+@R3         //Access RAM[3]
 D=M         //D=R3
-@R4
+@R4         //Access RAM[4]
 D=D&M       //D=R3 and R4 (which equal to: (x or y) and not(x and y))
-@R2      
+@R2         //Access RAM[2]
 M=D         //R2=(x or y) and not(x and y)
