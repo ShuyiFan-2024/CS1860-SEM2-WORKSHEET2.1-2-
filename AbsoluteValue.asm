8b484a4aeab91507098b6108d7b6f21ca050da0a
ShuyiFan-2024 <qnwc5470@leeds.ac.uk>
2025-05-07 20:26:43 +0000

    111

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -1,29 +1,29 @@
 //Initialize R2 and R3
 @R2       //Tell the CPU that it will access RAM[2]
 M=0      //R2=0, suppose x is negative
-@R3      //Access RAM[3]
+@R3      //Tell the CPU that it will access RAM[3]
 M=0      //R3=0, suppose that the absolute value of x can be calculated
 
 //Read X and check if it is negative
-@R0            //Access RAM[0]
+@R0            //Tell the CPU that it will access RAM[0]
 D=M             //D=R0(x)
 @NEGATIVE       //Jump to the destination
 D;JLT           //Jump condition, if x<0, jump to NEGATIVE
 
 //Handle the case where X is a positive number
 (POSITIVE)     //Label
-@R0           //Access RAM[0]
+@R0           //Tell the CPU that it will access RAM[0]
 D=M           //D=R0=x
-@R1           //Access RAM[1]
+@R1           //Tell the CPU that it will access RAM[1]
 M=D           //R1=x
 @END          //Jump to the destination
 0;JMP         //unconditional jump         
 
 //Handle the case where X is a negative number
 (NEGATIVE)    //Label
-@R2         //Access RAM[2]
+@R2         //Tell the CPU that it will access RAM[2]
 M=1         //R2=1, it indicates that x is negative
-@R0         //access RAM[0]
+@R0         //Tell the CPU that it will access RAM[0]
 D=M         //D=R0=x, read x
 @MIN_VALUE   //Jump to the destination
 D;JEQ        //Jump condition, if x==-32768, jump to MIN_VALUE
@@ -31,16 +31,16 @@ D;JEQ        //Jump condition, if x==-32768, jump to MIN_VALUE
 //Calculate the absolute value x (when x is negative)
 D=!D        //Take the opposite
 D=D+1       //and then add one
-@R1         //Access RAM[1]
+@R1         //Tell the CPU that it will access RAM[1]
 M=D       //R1=-x
 @END        //Jump to the destination
 0;JMP       //Unconditional jump
 
 //Handling special cases: x==-32768
 (MIN_VALUE)     //Label
-@R3         //Access RAM[3]
+@R3         //Tell the CPU that it will access RAM[3]
 M=1        //R3=1, it indicates that the absolute value x is out of range
-@R1         //Access RAM[1]
+@R1         //Tell the CPU that it will access RAM[1]
 M=0        //R1=0, since the absolute value x cannot be expressed
 
 (END)      //Label
--- Xor.asm
+++ Xor.asm
@@ -2,27 +2,27 @@
 // step1: compute x or y
 @R0            //Tell the CPU that it will access RAM[0]
 D=M            //D=R0(x)    
-@R1            //Access RAM[1]
+@R1            //Tell the CPU that it will access RAM[1]
 D=D|M         //D=R0 or R1 (x or y)  
 @R3           //the address of the R3 temporary variable,R3=x or y
 M=D           //R3=x or y
 //step2: compute x and y
-@R0           //Access RAM[0]
+@R0           //Tell the CPU that it will access RAM[0]
 D=M          //D=R0
-@R1           //Access RAM[1]
+@R1           //Tell the CPU that it will access RAM[1]
 D=D&M         //R0 and R1(x and y)
-@R4           //Access RAM[4]
+@R4           //Tell the CPU that it will access RAM[4]
 M=D           //R4=x and y
 //step3: compute not(x and y)
-@R4          //Access RAM[4]
+@R4          //Tell the CPU that it will access RAM[4]
 D=M          //D=R4
 D=!D        //D= not(x and y)
-@R4          //Access RAM[4]
+@R4          //Tell the CPU that it will access RAM[4]
 M=D          //R4= not(x and y)
 //step4: compute (x or y) and not(x and y)
-@R3         //Access RAM[3]
+@R3         //Tell the CPU that it will access RAM[3]
 D=M         //D=R3
-@R4         //Access RAM[4]
+@R4         //Tell the CPU that it will access RAM[4]
 D=D&M       //D=R3 and R4 (which equal to: (x or y) and not(x and y))
-@R2         //Access RAM[2]
+@R2         //Tell the CPU that it will access RAM[2]
 M=D         //R2=(x or y) and not(x and y)
